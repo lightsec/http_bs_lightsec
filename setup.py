72747f8195d26743d3f80eac4c43ea916bb5f825
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2014-11-10 16:06:04 +0100

    Adding installation scripts.

---
+++ setup.py
@@ -0,0 +1,31 @@
+'''
+Created on Nov 10, 2014
+
+@author: Aitor Gómez Goiri <aitor.gomez@deusto.es>
+
+To install/reinstall/uninstall the project and its dependencies using pip:
+     pip install ./
+     pip install ./ --upgrade
+     pip uninstall liblightsec
+'''
+from setuptools import setup #, find_packages
+
+setup(name="liblightsec",
+      version="0.1",
+      description="Sample HTTP server for a base station following the lightsec protocol.",
+      #long_description = "",
+      author = "Aitor Gomez-Goiri",
+      author_email = "aitor.gomez@deusto.es",
+      maintainer = "Aitor Gomez-Goiri",
+      maintainer_email = "aitor.gomez@deusto.es",
+      url = "https://github.com/lightsec/http_bs_lightsec",
+      #license = "http://www.apache.org/licenses/LICENSE-2.0",
+      platforms = ["any"],
+      package_dir = {
+        '': 'src',
+      },
+      packages = ["httplightsec"],
+      install_requires = [
+	"liblightsec"
+      ],
+)
