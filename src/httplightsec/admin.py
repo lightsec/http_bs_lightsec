6de86f68c87944dfe1d7719a330f4cb2d9550b94
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2014-11-29 15:15:36 +0100

    Adding a new Sensor model and a N-M relationship with the User model (both can be managed in the admin panel).

--- src/httplightsec/admin.py
+++ src/httplightsec/admin.py
@@ -5,9 +5,11 @@ Created on 29/11/2014
 '''
 
 from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
 from httplightsec.app import app, db
+from httplightsec.models import Sensor
 from httplightsec.views import UserView
 
 admin = Admin(app) # check /admin
 admin.add_view(UserView(db.session)) #, url="user"))
-#admin.add_view(ModelView(Sensor, db.session))
+admin.add_view(ModelView(Sensor, db.session))
--- src/httplightsec/models.py
+++ src/httplightsec/models.py
@@ -3,16 +3,22 @@ Created on 29/11/2014
 
 @author: Aitor Gomez Goiri <aitor.gomez@deusto.es>
 '''
+
 from httplightsec.app import db
+
 
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(120), unique=True)
+    has_access_to = db.relationship('Sensor', secondary=lambda: access_table)
 
     def __repr__(self):
         return '<User %r>' % self.username
+    
+    def __str__(self):
+        return self.username
     
         # Flask-Login integration
     def is_authenticated(self):
@@ -30,3 +36,25 @@ class User(db.Model):
 #new_user = User(...)
 #db.session.add(new_user)
 #db.session.commit()
+
+
+class Sensor(db.Model):
+    __tablename__ = 'sensor'
+    id = db.Column(db.Integer, primary_key=True)
+    auth_key = db.Column(db.String(120), unique=True)
+    enc_key = db.Column(db.String(120), unique=True)
+    auth_users = db.relationship('User', secondary=lambda: access_table)
+    #auth_user
+    
+    def __str__(self):
+        return 'Sensor%r' % self.id
+
+    def __repr__(self):
+        return '<Sensor %r>' % self.id
+    
+
+
+access_table = db.Table('access', db.Model.metadata,
+    db.Column('sensor_id', db.Integer, db.ForeignKey('sensor.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
