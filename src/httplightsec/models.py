4b7765332d0d59c6f196fc17b32eb97f0774926b
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2014-12-18 17:45:52 +0100

    Two users can have the same password.

--- src/httplightsec/models.py
+++ src/httplightsec/models.py
@@ -11,7 +11,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
     has_access_to = db.relationship('Sensor', secondary=lambda: access_table)
 
     def __repr__(self):
@@ -34,11 +34,6 @@ class User(db.Model):
         return self.id
 
 
-# new_user = User(...)
-#db.session.add(new_user)
-#db.session.commit()
-
-
 class Sensor(db.Model):
     __tablename__ = 'sensor'
     id = db.Column(db.Integer, primary_key=True)
@@ -46,7 +41,6 @@ class Sensor(db.Model):
     auth_key = db.Column(db.String(120))
     enc_key = db.Column(db.String(120))
     auth_users = db.relationship('User', secondary=lambda: access_table)
-    #auth_user
 
     def __str__(self):
         return 'Sensor%r' % self.id
